68eabb5aa8dc5086fab08cdf3b620a6b60a8dc08
Cheng Ching Jen <win50117@gmail.com>
2020-10-30 15:54:46 +0800

    feat:建立API Controller和Model

---
+++ RESTfulAPIPractice/Models/Quote.cs
@@ -0,0 +1,10 @@
+namespace RESTfulAPIPractice.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
