155cc6a29caa05ab591d06707205e092977ef370
Cheng Ching Jen <win50117@gmail.com>
2020-10-31 05:32:07 +0800

    feat:建立資料

--- RESTfulAPIPractice/Controllers/QuotesController.cs
+++ RESTfulAPIPractice/Controllers/QuotesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RESTfulAPIPractice.Models;
 
 namespace RESTfulAPIPractice.Controllers
 {
@@ -11,5 +12,9 @@ namespace RESTfulAPIPractice.Controllers
     [ApiController]
     public class QuotesController : ControllerBase
     {
+        List<Quote> quotes = new List<Quote>(){
+            new Quote(){Id = 0, Author = "Emily", Description = "The brain is wider than the sky", Title = "書1"},
+            new Quote(){Id = 1, Author = "Richard", Description = "True love stories never have endings.", Title = "書2"}
+        };
     }
 }
